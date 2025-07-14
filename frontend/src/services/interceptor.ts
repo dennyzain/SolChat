af6dd8695693798dcc07a42c57344c5f59ad4b30
dennyzain <abbasdenny24@gmail.com>
2025-07-14 11:10:46 +0700

    feat: implement JWT token handling in Axios interceptor

--- frontend/src/services/interceptor.ts
+++ frontend/src/services/interceptor.ts
@@ -1,8 +1,17 @@
 import axios from "axios"
+import { useAuthStore } from "../store";
 
 export const instance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 })
+
+instance.interceptors.request.use((config) => {
+    const token = useAuthStore.getState().jwtToken;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
 
 
 export default instance
