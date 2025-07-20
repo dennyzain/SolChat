8413e99edf840099d2012813dd003a87c3450d4c
dennyzain <abbasdenny24@gmail.com>
2025-07-20 21:48:22 +0700

    chore: production ready

--- backend/index.ts
+++ backend/index.ts
@@ -11,12 +11,15 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: process.env.FRONTEND_URL,
         methods: ["GET", "POST"]
     }
 });
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"]
+}));
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use(authenticateToken)
